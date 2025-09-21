d840e59bd89e7c1672adb9ca51ca2757efb93090
MRuhullah <ateebqazim11@gmail.com>
2025-09-21 11:05:45 +0530

    Sum and Product of Digits

---
+++ Digits.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, digit;
+    int sum = 0, product = 1;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    // Handle negative numbers
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+    int temp = num;
+
+    if (num == 0)
+    {
+        sum = 0;
+        product = 0;
+    }
+    else
+    {
+        while (num > 0)
+        {
+            digit = num % 10;
+            sum += digit;
+            product *= digit;
+            num /= 10;
+        }
+    }
+
+    cout << "Sum of digits of " << temp << " = " << sum << endl;
+    cout << "Product of digits of " << temp << " = " << product << endl;
+
+    return 0;
+}
