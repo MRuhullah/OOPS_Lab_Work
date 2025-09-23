57c96c98540e062d1082c3412b9c2f0bc28f6bc9
MRuhullah <ateebqazim11@gmail.com>
2025-09-23 18:10:51 +0530

    Reverse

---
+++ Reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, reversed = 0;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+     
+    cout << "Reversed number: " << reversed << endl;
+
+    return 0;
+}
