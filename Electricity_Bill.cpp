74b507e5850bd62081e35f3be77aa6042e29e5cd
MRuhullah <ateebqazim11@gmail.com>
2025-09-24 20:49:53 +0530

    Declared

---
+++ Electricity_Bill.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int Consumer_Number, Previous_Month_readings, Current_Month_readings;
+    string Consumer_Name, ER_Connection;
+
+    cout << "Please enter your Consumer Number" << endl;
+    cin >> Consumer_Number;
+
+    cout << "Please enter your Consumer Name" << endl;
+    cin >> Consumer_Name;
+
+    cout << "Which tyoe of connection do you have?" << endl;
+    cin >> ER_Connection;
+}
