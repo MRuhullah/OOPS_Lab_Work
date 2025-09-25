8f0a42bd7ffa27f0bf975d6648c4f05d9db9c6f3
MRuhullah <ateebqazim11@gmail.com>
2025-09-25 18:29:33 +0530

    Ele

--- Electricity_Bill.cpp
+++ Electricity_Bill.cpp
@@ -7,7 +7,7 @@ int main()
 {
 
     int Consumer_Number, Previous_Month_readings, Current_Month_readings;
-    string Consumer_Name, ER_Connection;
+    string Consumer_Name, ER_Connection,domestic,commercial;
 
     cout << "Please enter your Consumer Number" << endl;
     cin >> Consumer_Number;
@@ -15,6 +15,17 @@ int main()
     cout << "Please enter your Consumer Name" << endl;
     cin >> Consumer_Name;
 
-    cout << "Which tyoe of connection do you have?" << endl;
+    cout << "Enter previous month readings" << endl;
+    cin >> Previous_Month_readings;
+
+    cout << "Enter current month readings" << endl;
+    cin >> Current_Month_readings;
+
+    cout << "Which tyoe of connection do you have(IN SMALL)?" << endl;
     cin >> ER_Connection;
+
+    if(ER_Connection == domestic)
+    {
+        
+    }
 }
