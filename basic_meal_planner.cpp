55ee1fc768ac447b1a564585db45b0735cb55a80
MRuhullah <ateebqazim11@gmail.com>
2025-10-06 22:46:45 +0530

    Add: Problem

--- basic_meal_planner.cpp
+++ basic_meal_planner.cpp
@@ -3,50 +3,63 @@
 
 using namespace std;
 
-int main() {
-    string meals[7]; // For 7 days, one meal per day
+int main()
+{
+    string meals[7];
     int choice;
     int day;
     string meal;
 
-    do {
+    do
+    {
         cout << "\n=== Simple Meal Planner ===\n";
         cout << "1. Add Meal\n";
         cout << "2. View Meals\n";
         cout << "3. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
-        cin.ignore(); // To clear input buffer
+        cin.ignore();
 
-        if (choice == 1) {
+        if (choice == 1)
+        {
             cout << "Enter day number (1 for Monday to 7 for Sunday): ";
             cin >> day;
-            cin.ignore(); // Clear buffer again
+            cin.ignore();
 
-            if (day < 1 || day > 7) {
+            if (day < 1 || day > 7)
+            {
                 cout << "Invalid day number.\n";
-            } else {
+            }
+            else
+            {
                 cout << "Enter meal for day " << day << ": ";
                 getline(cin, meal);
                 meals[day - 1] = meal;
                 cout << "Meal saved.\n";
             }
-
-        } else if (choice == 2) {
+        }
+        else if (choice == 2)
+        {
             cout << "\n--- Weekly Meals ---\n";
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < 7; i++)
+            {
                 cout << "Day " << (i + 1) << ": ";
-                if (meals[i].empty()) {
+                if (meals[i].empty())
+                {
                     cout << "No meal set.\n";
-                } else {
+                }
+                else
+                {
                     cout << meals[i] << endl;
                 }
             }
-
-        } else if (choice == 3) {
+        }
+        else if (choice == 3)
+        {
             cout << "Goodbye!\n";
-
-        } else {
+        }
+        else
+        {
             cout << "Invalid choice. Try again.\n";
         }
 
