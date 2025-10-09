ca51250a3ff054fa1618fcb3be93ef0ee3e1bbbe
MRuhullah <ateebqazim11@gmail.com>
2025-10-09 18:50:23 +0530

    Add

---
+++ Arithmatic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string expression;
+
+    cout << "Enter the expression you wanna evaluate:" << endl;
+    getline(cin, expression);
+    cout << "The entered expression is: " << expression << endl;
+    
+}
